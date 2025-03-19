dd7c57854e6e8eb526a78b8dd601b57b82be8c4c
Hyun-009 <iovemint@naver.com>
2025-03-19 10:43:55 +0900

    Add files via upload

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = ({value,onClick,className=''}) => {
+    return (
+        <button 
+        className={`btn ${className}`}
+        onClick={()=>onClick(value)}
+        >
+            {value}</button>
+    )
+}
+
+export default Button
